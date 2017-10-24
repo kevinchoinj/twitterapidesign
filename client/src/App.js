456e8c563b15105895d30cad49640f5911a7fe2f
Kevin Choi <kevinchoinj@yahoo.com>
2017-10-24 15:41:56 -0400

    base twitter contents, import react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      twitt: [
+      {key:'0', value:{"text":"", "created_at":""}}, 
+      {key:'1', value:{"text":"", "created_at":""}},
+      {key:'2', value:{"text":"", "created_at":""}},
+    ]
+     }
+  }
+
+  componentDidMount() {
+    this.twitteroutput();
+  }
+
+  twitteroutput = () => {
+    fetch('/api/twitter')
+      .then(res => res.json())
+      .then(twitt => this.setState({ twitt }));
+  }
+
   render() {
+
+    const { twitt } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+{twitt.length ? (
+
+ <div>
+ {this.state.twitt["0"].value.created_at}
+ <br/>
+ {this.state.twitt["0"].value.text}
+ <br/><br/>
+ {this.state.twitt["1"].value.created_at}
+ <br/>
+ {this.state.twitt["1"].value.text}
+ <br/>
+ <br/>
+ {this.state.twitt["2"].value.created_at}
+ <br/>
+ {this.state.twitt["2"].value.text}
+ <br/><br/>
+ <button
+  className="more"
+  onClick={this.twitteroutput}>
+  Reload
+</button>
+</div>
+)
+:
+(
+<div>
+<button
+  onClick={this.twitteroutput}>
+  Reload
+</button>
+</div>
+)}
+
       </div>
     );
   }
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+var Twitter = require('twitter');
+
+try{
+	var json = require('./config.json');
+  var consumerkey= json.consumerkey;
+  var consumersecret = json.consumersecret;
+  var accesstoken = json.accesstoken;
+  var accesssecret = json.accesssecret;
+}
+catch(err){
+var consumerkey=process.env.REACT_APP_CON_KEY;
+var consumersecret = process.env.REACT_APP_CON_SEC;
+var accesstoken = process.env.REACT_APP_ACC_KEY;
+var accesssecret = process.env.REACT_APP_ACC_SEC;
+}
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+var client = new Twitter({
+  consumer_key: consumerkey,
+  consumer_secret: consumersecret,
+  access_token_key: accesstoken,
+  access_token_secret: accesssecret
+ });
+ var params = {screen_name: 'nodejs', count:3};
+
+app.get('/api/twitter', (req, res) => {
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+   if (!error) {
+    const tweethold = Object.keys(tweets).map
+      (function(k){return{key:k, value:tweets[k]}}
+    )
+    res.json(tweethold);
+   }
+   else{
+     console.log(error);
+   }
+  });
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server started on ${port}`);
+
+
+
+
+
+
